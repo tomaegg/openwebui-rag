278cc41b8d94d9f4c638149b25534ee22f4e2b21
scarlet <phantom190514@gmail.com>
2025-08-28 21:46:26 +0800

    feat: support open-webui tool server

--- rag/cmd/tool-server/main.go
+++ rag/cmd/tool-server/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"os"
 	"os/signal"
 	v1 "rag/generated/ragtools/v1"
 	"rag/generated/ragtools/v1/handler"
+	"rag/schemas"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -26,9 +28,20 @@ func gracefully() {
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"*"}, // 或 ["*"] 放行所有
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
 
 	h := handler.NewToolServer()
 	v1.RegisterHandlersWithBaseURL(e, h, "")
+	e.GET("/openapi.json", func(ctx echo.Context) error {
+		j := json.RawMessage(schemas.ToolsOpenAPI)
+		ctx.JSON(http.StatusOK, j)
+		return nil
+	})
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
---
+++ rag/schemas/schema.go
@@ -0,0 +1,8 @@
+package schemas
+
+import (
+	_ "embed"
+)
+
+//go:embed openapi.tools.json
+var ToolsOpenAPI []byte
